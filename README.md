77d780b1b38728267fb13c29e99f8d6cfdc7d455
Brian Aydemir <baydemir@morgridge.org>
2024-01-11 12:17:51 -0600

    Update the download script to filter out and log invalid urls

--- README.md
+++ README.md
@@ -9,3 +9,11 @@
 3. Install dependencies.
 
        poetry install --sync
+
+4. Run the download script, saving the output into a log file.
+
+       poetry run python3 -m macrostrat.nbmg_import.download 2>&1 | tee -a download.log
+
+   Check for errors.
+
+       grep -i error download.log
--- macrostrat/nbmg_import/download.py
+++ macrostrat/nbmg_import/download.py
@@ -6,6 +6,7 @@ import asyncio
 import logging
 import pathlib
 import sys
+from typing import Optional
 
 import aiohttp
 import anyio
@@ -16,18 +17,35 @@ CONCURRENCY = 1  # how many files to download concurrently
 DOWNLOAD_DIR = pathlib.Path("tmp/download")  # where to put downloaded files
 
 
-async def download_file(url: str) -> pathlib.Path:
+async def download_object(url: str) -> Optional[pathlib.Path]:
+    """
+    Downloads the given object, and returns the file it was written into.
+    """
     DOWNLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
     filename = DOWNLOAD_DIR / url.split("/")[-1]
 
+    logging.info("Downloading %s into %s", url, filename)
+
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
+            try:
+                response.raise_for_status()
+            except aiohttp.ClientResponseError:
+                logging.error("Not downloading %s due to an HTTP error", url)
+                return None
             async with await anyio.open_file(filename, "wb") as fp:
-                async for data in response.content.iter_chunked(1024):
+                async for data in response.content.iter_chunked(8 * 1024 * 1024):  # 8 MB
                     await fp.write(data)
 
     return filename
+
+
+def is_valid_url(url: str) -> bool:
+    """
+    Returns whether the given URL is one that we are interested in downloading.
+    """
+    return url.startswith("https://data.nbmg.unr.edu/Public/") and url.endswith(".zip")
 
 
 async def main() -> None:
@@ -35,19 +53,19 @@ async def main() -> None:
     response = requests.get(url_to_scrape)
     soup = bs4.BeautifulSoup(response.text, "html.parser")
     links = soup.find_all("a")
-    files = [link["href"] for link in links if link["href"].endswith(".zip")]
+    urls = [link["href"] for link in links if is_valid_url(link["href"])]
 
     while True:
-        current, files = files[:CONCURRENCY], files[CONCURRENCY:]
+        current, urls = urls[:CONCURRENCY], urls[CONCURRENCY:]
         if not current:
             break
-        await asyncio.gather(*[download_file(file) for file in current])
+        await asyncio.gather(*[download_object(url) for url in current])
 
 
 def entrypoint() -> None:
     try:
         logging.basicConfig(
-            format="%(asctime)s ~ %(message)s",
+            format="[%(asctime)s] %(levelname)s %(module)s:%(lineno)d %(message)s",
             level=logging.DEBUG,
         )
         asyncio.run(main())
